251246cc358fd13198897d0444cce01b2bf24ea5
Lynette <lynettecargo@Lynettes-Air.home>
2023-07-06 13:02:40 -0500

    git init

---
+++ app.js
@@ -0,0 +1,35 @@
+/* STORYBOOK CV */
+const porfolio = [
+    {
+        part: 1,
+        title: "Welcome",
+        text: "Hello!",
+        img: "",
+        options: [
+            {
+                right: "Next Page",
+                next: 2,
+            },
+            {
+                left: "Resumè",
+                next: 1,
+            }
+        ]
+    },
+    {
+        part: 2,
+        title: "About Me",
+        text: "Hello!",
+        img: "",
+        options: [
+            {
+                right: "Next Page",
+                next: 3,
+            },
+            {
+                left: "Resumè",
+                next: 2,
+            }
+        ]
+    },
+]
