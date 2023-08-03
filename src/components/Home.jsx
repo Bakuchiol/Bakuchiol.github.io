6a2c9205d6db7f9aa87bb007c661135e5bc83e9b
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-02 22:39:48 -0500

    added links, download pdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 // STYLE AT app.css
-// parallax
-import { Parallax } from 'react-parallax'
-
-import cliff from '../images/cliff.jpg'
 
 function Home() {
   return (
     <div id='homeTitle'>
-      <Parallax className='homePara' bgImage={cliff} strength={200}>
         <h4>HOME TITLE</h4>
         <h4>HOME TITLE</h4>
         <h4>HOME TITLE</h4>
-        <h4>HOME TITLE</h4>
-      </Parallax>
-        
+        <h4>HOME TITLE</h4>        
     </div>
   )
 }
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './aboutStyle.css'
+// 
+const resume = 'http://localhost:3000/Resume.pdf'
 
 function About() {
+  // resume download
+  const downloadCV = (url) => {
+    const fileName = url.split('/').pop()
+    const aTag = document.createElement('a')
+    aTag.href = url
+    aTag.setAttribute('download',fileName);
+    document.body.appendChild(aTag)
+    aTag.click();
+    aTag.remove();
+  }
+
   return (
     <div id='aboutWrapper'>
-      <h4>submission form</h4>
-      <h4>contact me/let's work together</h4>
+      <div className='aboutForm'>
+        <h2>FORM GOES HERE</h2>
+      </div>
+      <div className='aboutLinks'>
+        <h4>contact me/let's work together</h4>
+        <ul>
+          <li><a href='https://www.linkedin.com/in/lynette-anne-cargo/'>LinkedIn</a></li>
+          <li><a href='https://github.com/Bakuchiol'>GitHub</a></li>
+          <li onClick={()=>{downloadCV(resume)}}>Resume</li>
+        </ul>
+      </div>
     </div>
   )
 }
