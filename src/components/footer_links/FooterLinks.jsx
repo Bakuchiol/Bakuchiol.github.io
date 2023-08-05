f018cd7c71dc396186e2c357cd2a53ac1969e79b
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-05 10:57:09 -0500

    footer component

--- src/components/footer_links/FooterLinks.jsx
+++ src/components/footer_links/FooterLinks.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import './footerStyle.css'
+// 
+const resume = 'http://localhost:3000/Resume.pdf'
 
 function FooterLinks() {
-
+  // mail
   const Mail = ({email, subject, body, children}) => {
     return (
       <a href={`mailto:${email}?subject=${encodeURIComponent(subject) || ''}&body=${encodeURIComponent(body) || ''}`}>{children}</a>
     )
   }
+  // resume
+  // resume download
+  const downloadCV = (url) => {
+    fetch(url)
+      .then(response => response.blob())
+      .then(resume => {
+        const resumeURL = window.URL.createObjectURL(new Blob([resume]))
+        const fileName = url.split('/').pop()
+        const aTag = document.createElement('a')
+        aTag.href = resumeURL
+        aTag.setAttribute('download',fileName);
+        document.body.appendChild(aTag)
+        aTag.click();
+        // aTag.remove();
+    })
+  };
 
   return (
     <div id='footerLink'>
@@ -24,7 +42,11 @@ function FooterLinks() {
         </div>
         {/*  */}
         <div className="mailRight">
-          
+          <ul>
+            <li><a href='https://www.linkedin.com/in/lynette-anne-cargo/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
+            <li><a href='https://github.com/Bakuchiol' target='_blank' rel='noreferrer'>GitHub</a></li>
+            <li className='linkLink' onClick={()=>{downloadCV(resume)}}><a href={resume} target='_blank' rel='noreferrer'>Download Resume</a></li>
+          </ul>
         </div>
       </div>
     </div>
